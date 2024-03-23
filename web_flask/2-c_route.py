d05176cf5a84c94fd01a3342237d4f7b952f2749
Leedasamurai <lesegoleemac@gmail.com>
2024-03-23 03:11:15 -0700

    cin flask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -3,27 +3,28 @@
 Starts a Flask web application.
 """
 
-from flask import Flask, escape
+from flask import Flask
+app.url_map.strict_slashes = False
 
-app = Flask(__name__)
 
-
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
+@app.route('/')
+def hello_world():
+    """ Returns some text. """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """Displays 'HBNB'"""
+@app.route('/hbnb')
+def hello():
+    """ Return other text. """
     return 'HBNB'
 
-@app.route('/c/<text>', strict_slashes=False)
+
+@app.route('/c/<text>')
 def c_text(text):
     """ replace text with variable. """
     text = text.replace('_', ' ')
     return 'C {}'.format(text)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
